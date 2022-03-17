6421f840c2e687cc18443199ef2fcab48212bda8
ostnam <tsougaevm@gmail.com>
2022-03-17 21:37:24 +0100

    added benchmarking CLI flag to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use lib::{reference::affine_gap_align, validation_lib::AlignmentType, wavefront_alignment};
 use std::io::{stdin, BufRead};
+use std::time::Instant;
 
 /// Struct used for parsing CLI args with clap.
 #[derive(Parser, Debug)]
@@ -15,17 +16,20 @@ struct MainArgs {
     open_pen: i32,
     #[clap(short, long)]
     extd_pen: i32,
+
+    #[clap(short, long)]
+    bench: bool,
 }
 
 fn main() {
+    // parse CLI args
     let args = MainArgs::parse();
 
+    // read alignment strings from stdin
     let mut query: String = String::new();
     let mut text: String = String::new();
-
     stdin().lock().read_line(&mut query).unwrap();
     stdin().lock().read_line(&mut text).unwrap();
-
     query = query.trim().to_string();
     text = text.trim().to_string();
 
@@ -35,12 +39,23 @@ fn main() {
         extd_pen: args.extd_pen,
     };
 
+    let before = if args.bench {
+        Some(Instant::now())
+    } else {
+        None
+    };
+
     let alignment = match args.method {
         AlignmentType::WavefrontNaive => wavefront_alignment::wavefront_align(&query, &text, &pens),
         AlignmentType::WavefrontNaiveAdaptive => {
             wavefront_alignment::wavefront_align_adaptive(&query, &text, &pens)
         }
         AlignmentType::Reference => affine_gap_align(&query, &text, &pens),
+    };
+
+    if let Some(t) = before {
+        let elapsed = t.elapsed();
+        println!("Aligned in {:.2?}", elapsed);
     };
 
     match alignment {
